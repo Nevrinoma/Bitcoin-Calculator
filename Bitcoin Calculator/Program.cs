c41ddc47bcbcbf812fd4775a4fbdc323a6e78556
Opilane TTHK <opilane@tthk.ee>
2022-01-25 09:26:34 +0200

    Add project files.

---
+++ Bitcoin Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace Bitcoin_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bitcoinRate bitcoin  = GetRates();
+            Console.WriteLine($"Update time >>> ");
+            Console.WriteLine($"Curent rate in {bitcoin.bpi.USD.code}>>> {bitcoin.bpi.USD.rate_float}");
+            Console.WriteLine($"Curent rate in {bitcoin.bpi.EUR.code}>>> {bitcoin.bpi.EUR.rate_float}");
+            Console.WriteLine($"Curent rate in {bitcoin.bpi.GBP.code}>>> {bitcoin.bpi.GBP.rate_float}");
+
+        }
+        
+        public static bitcoinRate GetRates()
+
+        {
+            string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+            bitcoinRate bitcoin;
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                bitcoin = JsonConvert.DeserializeObject<bitcoinRate>(response);
+
+            }
+
+            return bitcoin;
+
+        }
+
+
+    }
+}
---
+++ Bitcoin Calculator/bitcoinRate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bitcoin_Calculator
+{
+    class bitcoinRate
+    {
+        public bpi bpi { get; set; }
+
+        public time time { get; set; }
+    }
+
+    public class bpi
+    {
+        public USD USD { get; set; }
+
+        public EUR EUR { get; set; }
+
+        public GBP GBP { get; set; }
+
+    }
+
+
+    public class USD
+    {
+        public string code { get; set; }
+        public float rate_float { get; set; }
+    }
+    public class EUR
+    {
+        public string code { get; set; }
+        public float rate_float { get; set; }
+    }
+    public class GBP
+    {
+        public string code { get; set; }
+        public float rate_float { get; set; }
+    }
+}
